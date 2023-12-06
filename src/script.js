d42483b8bfbd6f2d94f6ad23f013a61057e61b1b
Szymon Wilczek <kazikwilczek7@gmail.com>
2023-12-06 20:11:12 +0100

    Finished up project

--- src/script.js
+++ src/script.js
@@ -4,14 +4,14 @@ import Experience from "./Experience/Experience.js";
 
 const experience = new Experience(document.querySelector("canvas.webgl"));
 
-const image = document.querySelector("img");
-var touchDevice = "ontouchstart" in window;
+// const image = document.querySelector("img");
+// var touchDevice = "ontouchstart" in window;
 
-if (touchDevice === false) {
-  if (image) {
-    image.style.visibility = "visible";
-    image.setAttribute("draggable", false);
-    //set atrivute to image to not be selectable
-    image.setAttribute("unselectable", "on");
-  }
-}
+// if (touchDevice === false) {
+//   if (image) {
+//     image.style.visibility = "visible";
+//     image.setAttribute("draggable", false);
+//     //set atrivute to image to not be selectable
+//     image.setAttribute("unselectable", "on");
+//   }
+// }
